b04e09159f8d66cfe916433db024a02600840c19
babak <behzadi.babak@gmail.com>
2023-05-17 16:59:31 +0330

    Initial commit

---
+++ src/main/java/com/babak/gerimedicaassignment/GerimedicaAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.babak.gerimedicaassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerimedicaAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GerimedicaAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/babak/gerimedicaassignment/GerimedicaAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babak.gerimedicaassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerimedicaAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
